7ce180a479273d5b3b17d947bb28bf12ff81ef59
laminebarghouda <47278306+laminebarghouda@users.noreply.github.com>
2019-06-18 21:53:51 +0100

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,25 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(int x1, int y1, int x2, int y2) {
+    this->x1=x1;
+    this->y1=y1;
+    this->x2=x2;
+    this->y2=y2;
+}
+
+int Rectangle::getX1() {
+    return this->x1;
+}
+
+int Rectangle::getY1() {
+    return this->y1;
+}
+
+int Rectangle::getX2() {
+    return this->x2;
+}
+
+int Rectangle::getY2() {
+    return this->y2
+    ;
+}
